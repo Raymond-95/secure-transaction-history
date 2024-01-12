e1445b759644185ca3338c9157ef598fbcb1acb7
Raymond <lauweiyan_95@hotmail.com>
2024-01-12 14:21:07 +0800

    [FEAT] mock the specific status code

--- src/mock/server.ts
+++ src/mock/server.ts
@@ -1,19 +1,22 @@
-import { createServer } from "miragejs";
+import { createServer, Response } from "miragejs";
 import { TransactionData } from 'mock/transactionData';
 
 export const makeServer = () => {
   return createServer({
     routes() {
-       // Set a urlPrefix to handle requests with "/api" prefix
-       this.namespace = "/api";
-       this.urlPrefix = 'http://localhost:8081';
-       this.post("/retrieveTransactionData", (schema, request) => {
-         // Handle the POST request logic here
+      // Set a urlPrefix to handle requests with "/api" prefix
+      this.namespace = "/api";
+      this.urlPrefix = 'http://localhost:8081';
+      this.post("/retrieveTransactionData", (schema, request) => {
+        // Handle the POST request logic here
          return {
            status: "success",
            data: { transactionData: TransactionData },
          };
-       });
+
+        // // throw specific error code
+        // return new Response(400, {}, { error: 'Bad Request' });
+      });
     },
   });
 };
